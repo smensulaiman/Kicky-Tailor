176b3440bc41c446fc2e9520f32ad65e2c2fb7c8
Mohammad Solaiman <smensulaiman007@gmail.com>
2020-04-11 15:51:22 +0600

    project-creation

---
+++ app/src/main/java/com/ekattorit/kickytailor/activities/dashboard/MainDashboardActivity.java
@@ -0,0 +1,16 @@
+package com.ekattorit.kickytailor.activities.dashboard;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.ekattorit.kickytailor.R;
+
+public class MainDashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_dashboard);
+    }
+}
---
+++ app/src/main/java/com/ekattorit/kickytailor/activities/welcome/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.ekattorit.kickytailor.activities.welcome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ekattorit.kickytailor.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
